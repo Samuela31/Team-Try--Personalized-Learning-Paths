8de94b272fb7bb52001fb817c45fc6a22766043b
Samuela31 <94887862+Samuela31@users.noreply.github.com>
2024-02-04 00:13:50 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,23 @@
+import pathlib
+import textwrap
+import os
+import google.generativeai as genai
+from IPython.display import display
+from IPython.display import Markdown
+
+'''def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))'''
+
+def gemini(text):
+  GOOGLE_API_KEY=os.getenv('GOOGLE_API_KEY')
+  genai.configure(api_key=GOOGLE_API_KEY)
+
+  model = genai.GenerativeModel('gemini-pro')
+  chat = model.start_chat(history=[])
+
+  response = chat.send_message('%s -- Please answer as concisely as you can, avoiding any extra conversation or text. DO NOT USE <b> tag for bold, please use ** instead' % text)
+  gemini_response = response.text
+
+  return gemini_response
+
